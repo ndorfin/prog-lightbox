56b15cd1b3bd023820cdf5c194860451baff494e
Shaun O’Connell <ndorfin+github@gmail.com>
2024-08-02 14:33:06 +1200

    Update examples. Make the script more resilient.

--- mjs/prog-lightbox.mjs
+++ mjs/prog-lightbox.mjs
@@ -1,40 +1,53 @@
 class ProgLightbox extends HTMLElement {
 	#anchorElem;
-	#fullImageURL;
+	#imageURL;
 	#dialogElem;
 
 	#createDialog() {
-        let templateElem = document.querySelector('template[data-matches="prog-lightbox"]');
-        if (templateElem) {
-            let dialogTemplate = templateElem.content.children[0].cloneNode(true);
-            dialogTemplate.querySelector('dialog').setAttribute('id', this.cleanId);
-            dialogTemplate.querySelector('img').setAttribute('src', this.#fullImageURL);
-            dialogTemplate.querySelector('img').setAttribute('alt', this.alt);
-            this.#dialogElem = dialogTemplate;
-            document.body.append(this.#dialogElem);
-        }
+		if (this.currentDialog) {
+			this.#dialogElem = this.currentDialog;
+			return;
+		}
+
+		let templateElem = document.querySelector('template[data-matches="prog-lightbox"]');
+		if (templateElem) {
+			let dialogTemplate = templateElem.content.cloneNode(true);
+			dialogTemplate.querySelector('dialog').setAttribute('id', this.cleanId);
+			dialogTemplate.querySelector('img').setAttribute('src', this.#imageURL);
+			dialogTemplate.querySelector('img').setAttribute('alt', this.alt);
+			document.body.append(dialogTemplate);
+			this.#dialogElem = this.currentDialog;
+		}
 	}
 
 	#handleAnchorClick(event) {
 		event.preventDefault();
-		
-        if (!this.#dialogElem) this.#createDialog();
+		if (!this.#dialogElem) this.#createDialog();
 		this.#dialogElem.showModal();
 	}
 
 	connectedCallback() {
 		this.#anchorElem = this.querySelector('a');
 		this.#anchorElem.addEventListener('click', this.#handleAnchorClick.bind(this));
-		this.#fullImageURL = this.#anchorElem.getAttribute('href');
+		this.#imageURL = this.#anchorElem.getAttribute('href');
 	}
 
-    get alt() {
-        return this.getAttribute('alt');
-    }
+	get currentDialog() {
+		return document.querySelector(`#${this.cleanId}`);
+	}
 
-    get cleanId() {
-        return `dialog_${this.#fullImageURL.replace(/\W/g,'_')}`;
-    }
+	get alt() {
+		let altText = this.getAttribute('alt') || '';
+		if (altText) return altText;
+
+		const thumbnail = this.querySelector('img[alt]');
+		if (thumbnail) altText = thumbnail.getAttribute('alt') || '';
+		return altText;
+	}
+
+	get cleanId() {
+		return `dialog_${this.#imageURL.replace(/\W/g, '_')}`;
+	}
 }
 
 export default ProgLightbox;
